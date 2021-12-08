7571a9ea9eed3680a8dd00d50d3b7f37dcdadbdf
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-12-08 13:16:02 -0800

    added conftest.py and requirements.txt

---
+++ conftest.py
@@ -0,0 +1,58 @@
+import pytest
+from selenium import webdriver
+# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+# from webdriver_manager.opera import OperaDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", "-B", action="store", default="chrome", help="choose your browser")
+    parser.addoption("--language", "-L", action="store", default="en", help="choose language:en, ru, ...(etc)")
+    # parser.addoption("--url", "-U", action="store", default="http://localhost/opencart", help="choose your browser")
+
+
+@pytest.fixture
+def browser(request):
+    browser_param = request.config.getoption("--browser")
+    user_language = request.config.getoption("--language")   #  try test_fixture3.py for example to use  #stepik_3.6 step#8
+    if browser_param == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        driver = webdriver.Chrome('/Users/sergeysereda/dev/webdrivers/chromedriver', options=options)
+    elif browser_param == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        driver = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise Exception(f"{request.param} is not supported!")
+
+    driver.implicitly_wait(10)
+    request.addfinalizer(driver.close)
+    # driver.get(request.config.getoption("--url"))
+
+    return driver
+
+
+
+### option fixture without a language selection option
+#
+# @pytest.fixture
+# def browser(request):
+#     browser_param = request.config.getoption("--browser")
+#     if browser_param == "chrome":
+#         driver = webdriver.Chrome('/Users/sergeysereda/dev/webdrivers/chromedriver')
+#     elif browser_param == "firefox":
+#         driver = webdriver.Firefox()
+#     elif browser_param == "edge":
+#         driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+#     elif browser_param == "opera":
+#         driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+#     else:
+#         raise Exception(f"{request.param} is not supported!")
+#
+#     driver.implicitly_wait(10)
+#     request.addfinalizer(driver.close)
+#     driver.get(request.config.getoption("--url"))
+#
+#     return driver
+
