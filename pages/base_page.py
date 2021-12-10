84a6da70cda054ab8a1434c1ad2190886d9c00c2
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2021-12-09 17:06:08 -0800

    created assertion should_be_login_link in MainPage and NoSuchElementException by method is_element_present in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,33 @@
 
-class BasePage():
+
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+
+class BasePage():              ## (similar like by barancev's Applicaton)
     def __init__(self, browser, url):
-        self.browser = browser
+        self.browser = browser   ## browser inited in conftest.py
         self.url = url
+
+
 
     def open(self):
         self.browser.get(self.url)
+
+
+
+
+    def __init__(self, browser, url, timeout=3):   ## 4_2 step 6
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+
+
+    def is_element_present(self, how, what):    ## 4_2 step 6
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
